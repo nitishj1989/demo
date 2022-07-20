912e2dfd972c53efb18c310046bc86e7bcd10790
Nitish Joshi <nitjoshi@svb.com>
2022-07-20 15:41:23 +0530

    done

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-provider aws {
-  region = "us-west-2"
-}
-provider aws {
-  region = "us-east-1"
-  alias  = "us-east-1"
-}
-
-
 resource "aws_s3_bucket" "onebucket" {
    bucket = "testing-s3-with-terraform-njoi"
    acl = "private"
